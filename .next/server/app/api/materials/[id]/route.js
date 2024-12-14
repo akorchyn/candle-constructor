94dea208b45258e573a72ff5564c75b3343600bc
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-12-14 19:22:45 +0000

    removed next

--- .next/server/app/api/materials/[id]/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/materials/[id]/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_b0b694._.js");
-runtime.loadChunk("server/chunks/[root of the server]__5aca79._.js");
-runtime.loadChunk("server/chunks/_9ebe89._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/materials/[id]/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/materials/[id]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GX9EtaPIhhcPzF0AugFlhM0bsgkbbTmyxNxIjuy9OBE=\"\n}"

8282ac133655569f767ac49d1e63fcc95f45b06c
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-03-22 22:15:44 +0200

    something wrong with a cookies

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
+import { nextCookies } from "better-auth/next-js"
 
 export const authClient = createAuthClient({
     plugins: [
-        adminClient()
+        adminClient(),
+        nextCookies()
     ]
 })
